76f977449b4b383a0aa223c26ac7aadd438bc7c2
KIKI007 <qiqiustc@gmail.com>
2017-05-02 20:24:55 -0700

    A good version

---
+++ cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/DependInfo.cmake
@@ -0,0 +1,88 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/ext/nanovg/src/nanovg.c" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/ext/nanovg/src/nanovg.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CORO_SJLJ"
+  "NANOGUI_BUILD"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/nanogui/ext/eigen"
+  "../external/nanogui/ext/glfw/include"
+  "../external/nanogui/ext/nanovg/src"
+  "../external/nanogui/include"
+  "libigl/nanogui"
+  "../external/nanogui/ext/coro"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/nanogui_resources.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/nanogui_resources.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/button.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/button.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/checkbox.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/checkbox.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/colorpicker.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/colorpicker.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/colorwheel.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/colorwheel.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/combobox.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/combobox.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/common.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/common.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/darwin.mm" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/darwin.mm.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/glcanvas.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/glcanvas.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/glutil.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/glutil.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/graph.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/graph.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/imagepanel.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/imagepanel.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/imageview.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/imageview.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/label.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/label.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/layout.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/layout.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/messagedialog.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/messagedialog.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/popup.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/popup.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/popupbutton.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/popupbutton.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/progressbar.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/progressbar.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/screen.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/screen.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/serializer.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/serializer.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/slider.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/slider.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/stackedwidget.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/stackedwidget.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/tabheader.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/tabheader.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/tabwidget.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/tabwidget.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/textbox.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/textbox.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/theme.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/theme.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/vscrollpanel.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/vscrollpanel.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/widget.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/widget.cpp.o"
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/external/nanogui/src/window.cpp" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/CMakeFiles/nanogui-obj.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CORO_SJLJ"
+  "NANOGUI_BUILD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/nanogui/ext/eigen"
+  "../external/nanogui/ext/glfw/include"
+  "../external/nanogui/ext/nanovg/src"
+  "../external/nanogui/include"
+  "libigl/nanogui"
+  "../external/nanogui/ext/coro"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/nanogui_resources.h" "/Users/wangziqi/Desktop/USC Spring/Code/Supporter/cmake-build-release/libigl/nanogui/nanogui_resources.cpp"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
