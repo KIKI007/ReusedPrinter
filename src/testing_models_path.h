622f1872fb2db4e8ceb731230c855443252d1dbb
xuphoenix <yxu195@usc.edu>
2017-05-28 15:14:45 -0700

    xu

--- src/testing_models_path.h
+++ src/testing_models_path.h
@@ -1,11 +1,15 @@
 #ifndef testing_models_path_h_included
 #define testing_models_path_h_included
-
+#define Xuyang
 #ifndef TESTING_MODELS_PATH
 #ifdef _WIN32
+#ifdef Xuyang
+#define TESTING_MODELS_PATH "C:/Users/Administrator/Dropbox/Yang_Ziqi/Printing Model"
+#elif
 #define TESTING_MODELS_PATH "C:/Users/Administrator/Ziqi/Dropbox/Yang_Ziqi/Printing Model"
+#endif
 #elif __APPLE__
-	#define TESTING_MODELS_PATH "/Users/wangziqi/Dropbox/Yang_Ziqi/Printing Model"
+#define TESTING_MODELS_PATH "/Users/wangziqi/Dropbox/Yang_Ziqi/Printing Model"
 #endif
 #endif
 
